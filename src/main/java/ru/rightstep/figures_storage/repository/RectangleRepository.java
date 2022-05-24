567fba37ad323dda642b4b220dccd6fee68d72ce
Vladimir Malzam <v.malzam@gmail.com>
2022-05-24 23:11:57 +0300

    Add FigureRepository wish JPQL Queryes and refactoring

--- src/main/java/ru/rightstep/figures_storage/repository/RectangleRepository.java
+++ src/main/java/ru/rightstep/figures_storage/repository/RectangleRepository.java
@@ -1,13 +1,8 @@
 package ru.rightstep.figures_storage.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import ru.rightstep.figures_storage.model.Color;
 import ru.rightstep.figures_storage.model.Rectangle;
 
-import java.util.List;
-
 public interface RectangleRepository extends JpaRepository<Rectangle, Integer> {
-
-    List<Rectangle> findAllByColor(Color color);
 
 }
--- src/main/java/ru/rightstep/figures_storage/service/FigureService.java
+++ src/main/java/ru/rightstep/figures_storage/service/FigureService.java
@@ -3,34 +3,25 @@ package ru.rightstep.figures_storage.service;
 import org.springframework.stereotype.Service;
 import ru.rightstep.figures_storage.model.Color;
 import ru.rightstep.figures_storage.model.Figure;
-import ru.rightstep.figures_storage.repository.CircleRepository;
-import ru.rightstep.figures_storage.repository.RectangleRepository;
+import ru.rightstep.figures_storage.repository.FigureRepository;
 
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
 @Service
 public class FigureService {
-    private final CircleRepository circleRepository;
-    private final RectangleRepository rectangleRepository;
+    private final FigureRepository figureRepository;
 
-    public FigureService(CircleRepository circleRepository, RectangleRepository rectangleRepository) {
-        this.circleRepository = circleRepository;
-        this.rectangleRepository = rectangleRepository;
+    public FigureService(FigureRepository figureRepository) {
+        this.figureRepository = figureRepository;
     }
 
     public List<Figure> getFigures() {
-        List<Figure> figures = new ArrayList<>();
-        figures.addAll(circleRepository.findAll());
-        figures.addAll(rectangleRepository.findAll());
-        return figures;
+        return figureRepository.findAll();
     }
 
     public List<Figure> getFiguresWishColorOrderedByArea(Color color) {
-        List<Figure> figures = new ArrayList<>();
-        figures.addAll(circleRepository.findAllByColor(color));
-        figures.addAll(rectangleRepository.findAllByColor(color));
+        List<Figure> figures = figureRepository.findAllByColor(color);
         figures.sort(Comparator.comparing(Figure::getArea));
         return figures;
     }
