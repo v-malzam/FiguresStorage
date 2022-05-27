0dcdf00884bf5b57aae38cb11ca0f3684d3b578e
Vladimir Malzam <v.malzam@gmail.com>
2022-05-27 21:42:51 +0300

    Move OrderBy to DB

--- src/main/java/ru/rightstep/figures_storage/repository/CircleRepository.java
+++ src/main/java/ru/rightstep/figures_storage/repository/CircleRepository.java
@@ -3,6 +3,10 @@ package ru.rightstep.figures_storage.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.rightstep.figures_storage.model.Circle;
 
+import java.util.List;
+
 public interface CircleRepository extends JpaRepository<Circle, Integer> {
+
+    List<Circle> findByOrderByRadiusAsc();
 
 }
--- src/main/java/ru/rightstep/figures_storage/service/impl/CircleService.java
+++ src/main/java/ru/rightstep/figures_storage/service/impl/CircleService.java
@@ -5,7 +5,6 @@ import ru.rightstep.figures_storage.model.Circle;
 import ru.rightstep.figures_storage.repository.CircleRepository;
 import ru.rightstep.figures_storage.service.ICircleService;
 
-import java.util.Comparator;
 import java.util.List;
 
 @Service
@@ -18,8 +17,6 @@ public class CircleService implements ICircleService {
 
     @Override
     public List<Circle> getCirclesOrderedByRadius() {
-        List<Circle> circles = circleRepository.findAll();
-        circles.sort(Comparator.comparing(Circle::getRadius));
-        return circles;
+        return circleRepository.findByOrderByRadiusAsc();
     }
 }
